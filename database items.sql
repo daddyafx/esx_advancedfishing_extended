85f2a0f9aa89b8afd5aaa9d229bd11987d028df4
daddyafx <38339912+daddyafx@users.noreply.github.com>
2020-12-10 11:57:01 -0500

    Update database items.sql
    added new items to sql

--- database items.sql
+++ database items.sql
@@ -3,3 +3,9 @@ INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('fi
 INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('shark', 'Shark', -1, 0, 1);
 INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('turtle', 'Sea Turtle', 3, 0, 1);
 INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('turtlebait', 'Turtle Bait', 10, 0, 1);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('stingray', 'Stingray', 5, 0, 1);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('dolphin', 'Dolphin', 3, 0, 1);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('sharktiger', 'Tiger Shark', 3, 0, 1);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('sharkhammer', 'Hammerhead Shark', 3, 0, 1);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('killerwhale', 'Killer Whale', 2, 0, 1);
+
