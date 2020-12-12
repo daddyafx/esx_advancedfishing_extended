3f882d1da18c3cd59f6398b4149d7a114bcc4518
daddyafx <38339912+daddyafx@users.noreply.github.com>
2020-12-11 20:59:17 -0500

    Added "fish" to sql

--- database items.sql
+++ database items.sql
@@ -8,4 +8,5 @@ INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('do
 INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('sharktiger', 'Tiger Shark', 3, 0, 1);
 INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('sharkhammer', 'Hammerhead Shark', 3, 0, 1);
 INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('killerwhale', 'Killer Whale', 2, 0, 1);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('fish', 'Fish', 100, 0, 1);
 
